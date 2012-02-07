4918be57f289eac293d25a46fde4ca09f794cdee
Greggory Rothmeier <greggroth@gmail.com>
2012-02-07 16:10:28 -0500

    added comments and documented the usage

--- README.md
+++ README.md
@@ -4,3 +4,18 @@ I wrote this to obsessively check a website for changes, particularly whether th
 
 ## Requires
 This uses [RVM](http://beginrescueend.com/) and a .rvmrc file to ensure that ruby version 1.9.3 is used.
+
+## Usage
+First, run
+
+    bundle
+
+to install all of the required gems.  Then, update the ./lib/check_web.rb so that it grabs the part a page you want it to monitor.  Also **update the mailer options** so that it sends an e-mail to the correct address.  Next, edit ./config/schedule.rb to have it run at the frequency you want (this is for the [whenever](https://github.com/javan/whenever) gem).  Lastly, run
+
+    whenever
+
+to make sure the crontab looks right then
+
+    whenever -w
+
+to update the crontab.
--- lib/check_web.rb
+++ lib/check_web.rb
@@ -3,9 +3,12 @@ require "nokogiri"
 require "pony"
 require "CGI"
 
+####  User updatable  ####
+# update the next three lines to grab the element from the page you want to monitor
 url = "https://authors.aps.org/cgi-bin/wvman?acc=LP12682&auth=Rothmeier"
 status_filename = './lib/status'
 current_status = Nokogiri::HTML(open(url)).xpath("//br/following-sibling::text()").first.to_html.gsub(/\n/, '')
+##########################
 
 File.new(status_filename, "w") unless File::exists?(status_filename)
 last_status = IO.readlines(status_filename)
@@ -15,10 +18,12 @@ if current_status == last_status[0]
 else
   puts "[#{Time.now}]   New Status:  #{current_status}"
   Pony.mail(
+    ##########  User Updatable   ###########
       to: 'greggroth@gmail.com', 
       from: 'greggroth@gmail.com', 
       subject: 'Your Paper Status Has Changed!', 
       body: "New Status!!   #{CGI.unescapeHTML(current_status)}\n#{url}",
       via: :sendmail)
+    #########################################
   IO.write(status_filename, current_status)
 end
